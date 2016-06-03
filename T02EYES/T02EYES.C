19c749e7f5474892b2b193c93cbcddba66451199
Maxim Markov (MM3) <max.markov@mail.ru>
2016-06-03 17:37:38 +0400

    First realise

---
+++ T02EYES/T02EYES.C
@@ -0,0 +1,154 @@
+/* FILE NAME: T02EYES.C
+ * PROGRAMMER: MM3
+ * DATE: 02.06.2016
+ * PURPOSE: WinAPI windowed application sample.
+ */
+
+#include <stdlib.h>
+#include <math.h>
+
+#include <windows.h>
+
+/* Window class name */
+#define WND_CLASS_NAME "My Window Class"
+
+/* Forward references */
+LRESULT CALLBACK MyWinFunc( HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam );
+
+VOID DrawEye(HWND hWnd, HDC hDC, INT X, INT Y, INT R, INT R1);
+
+/* The main program function */
+INT WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                    CHAR *CmdLine, INT CmdShow )
+{
+  WNDCLASS wc;
+  HWND hWnd;
+  MSG msg;
+
+  /* Register window class */
+  wc.style = CS_HREDRAW | CS_VREDRAW;
+  wc.cbClsExtra = 0;
+  wc.cbWndExtra = 0;
+  wc.hCursor = LoadCursor(NULL, IDC_HAND);
+  wc.hIcon = LoadIcon(NULL, IDI_EXCLAMATION);
+  wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+  wc.hInstance = hInstance;
+  wc.lpszClassName = WND_CLASS_NAME;
+  wc.lpszMenuName = NULL;
+  wc.lpfnWndProc = MyWinFunc;
+
+  if (!RegisterClass(&wc))
+  {
+    MessageBox(NULL, "Error register window class", "ERROR", MB_OK | MB_ICONERROR);
+    return 0;
+  }
+
+  /* Create window */
+  hWnd = CreateWindow(WND_CLASS_NAME,
+    "My eyes realise",
+    WS_OVERLAPPEDWINDOW,
+    CW_USEDEFAULT, CW_USEDEFAULT,
+    1000, 1900,
+    NULL, NULL, hInstance, NULL);
+  if (hWnd == NULL)
+  {
+    MessageBox(NULL, "Create window erroe", "ERROR", MB_OK | MB_ICONERROR);
+    return 0;
+  }
+
+  /* Show window */
+  ShowWindow(hWnd, CmdShow);
+  UpdateWindow(hWnd);
+
+  /* Run message loop */
+  while (GetMessage(&msg, NULL, 0, 0))
+    DispatchMessage(&msg);
+
+  return 30;
+} /* End of 'WinMain' function */
+
+/* Window message handle function */
+LRESULT CALLBACK MyWinFunc( HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam )
+{
+  INT i, j, r;
+  HDC hDC;
+  PAINTSTRUCT ps;
+  static INT w, h;
+  static BITMAP bm;
+  static HBITMAP hBm, hBmLogo;
+  static HDC hMemDC, hMemDCLogo;
+
+    switch (Msg)
+  {
+  case WM_CREATE:
+    SetTimer(hWnd, 30, 10, NULL);
+    hBmLogo = LoadImage(NULL, "GLOBE.BMP", IMAGE_BITMAP, 0, 0,
+        LR_LOADFROMFILE);
+    GetObject(hBmLogo, sizeof(bm), &bm);
+    hDC = GetDC(hWnd);
+    hMemDC = CreateCompatibleDC(hDC);
+    hMemDCLogo = CreateCompatibleDC(hDC);
+    SelectObject(hMemDCLogo, hBmLogo);
+    ReleaseDC(hWnd, hDC);
+    return 0;
+  case WM_SIZE:
+    w = LOWORD(lParam);
+    h = HIWORD(lParam);
+    if (hBm != NULL)
+      DeleteObject(hBm);
+    hDC = GetDC(hWnd);
+    hBm = CreateCompatibleBitmap(hDC, w, h);
+    ReleaseDC(hWnd, hDC);
+    SelectObject(hMemDC, hBm);
+    SendMessage(hWnd, WM_TIMER, 0, 0);
+    return 0;
+  case WM_TIMER:
+    Rectangle(hMemDC, 0, 0, w + 1, h + 1);
+    BitBlt(hMemDC, 0, 0, bm.bmWidth, bm.bmHeight,
+      hMemDCLogo, 0, 0, SRCCOPY);
+    for (j = 0; j < 100; j++)
+    {
+      r = 50;
+      DrawEye(hWnd, hMemDC, r + rand() % 1800, r + rand() % 900, r, 15);
+    }
+    srand(30);
+    SetBkMode(hMemDC, TRANSPARENT);
+    SetTextColor(hMemDC, RGB(255, 0, 0));
+/*   TextOut(hMemDC, 30, 30, "30!", 3);   */
+    InvalidateRect(hWnd, NULL, FALSE);
+    return 0;
+  case WM_PAINT:
+    hDC = BeginPaint(hWnd, &ps);
+    BitBlt(hDC, 0, 0, w, h, hMemDC, 0, 0, SRCCOPY);
+    EndPaint(hWnd, &ps);
+    return 0;
+  case WM_DESTROY:
+    KillTimer(hWnd, 30);
+    DeleteDC(hMemDC);
+    DeleteObject(hBm);
+    PostQuitMessage(0);
+    return 0;
+  }
+  return DefWindowProc(hWnd, Msg, wParam, lParam);
+} /* End of 'MyWinFunc' function */
+
+VOID DrawEye(HWND hWnd, HDC hDC, INT X, INT Y, INT R, INT R1)
+{
+  POINT pt;
+  INT dx, dy;
+  DOUBLE a;
+  GetCursorPos(&pt);
+  ScreenToClient(hWnd, &pt);
+
+  Ellipse(hDC, X - R, Y - R, X + R, Y + R); 
+  dx = pt.x - X;
+  dy = pt.y - Y;
+  a = (R - R1) /sqrt(dx * dx + dy * dy);
+  if (a < 1)
+  {
+    dx *= a;
+    dy *= a;
+  }
+  Ellipse(hDC, X + dx - R1, Y + dy - R1, X + dx + R1, Y + dy + R1); 
+}
+/* END OF 'T02EYES.C' FILE */
